81c721c0ea67863a45b2daff38193e779efafe05
Karolina Stanik <karolina.stanik000@gmail.com>
2018-01-20 12:18:23 +0100

    new FlightsBundle

--- Dream-Flights/src/FlightsBundle/FlightsBundle.php
+++ Dream-Flights/src/FlightsBundle/FlightsBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace FlightsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class FlightsBundle extends Bundle
 {
 }
